b658cce14bef7505e8ecf622a7ba6ac0c5bb95e7
IreneByron <zhangbingqing7@huawei.com>
2020-01-22 12:01:06 +0800

    add progress

--- src/views/pages/videoDetail.jsx
+++ src/views/pages/videoDetail.jsx
@@ -15,19 +15,59 @@ export default class VideoList extends BaseComponent {
 	}
 
 	componentDidMount() {
+		let player = document.getElementById("player");
+		let vedioPause = document.getElementById("videoPause");
+		//播放 暂停
+		player.onclick = () => {
+			if (player.paused) {
+				player.play()
+				vedioPause.style.display = "none";
+			} else {
+				player.pause();
+				vedioPause.style.display = "block"
+			}
+		}
+		//拖动进度条
+		let bar = document.getElementById("bar")
+		bar.onclick = (e) => {
+			let time = player.duration * (e.offsetX / bar.width);
+			console.log(time)
+			player.currentTime = time;
+		}
 
+		//更新播放进度
+		let line = document.getElementById("line")
+		let current = document.getElementById("current");
+		player.addEventListener("timeupdate", () => {
+			//更新进度条
+			let pValue = (player.currentTime / player.duration) * 100;
+			line.style.width = `${pValue}%`
+			//显示当前播放进度的时间
+			current.innerText = this.getTimeBySecond(player.currentTime);
+		})
+
+		//播放结束时
+		player.addEventListener("ended", () => {
+			line.style.width = 0;
+			player.currentTime = 0;
+			//视频播放状态为设置为停止
+			player.pause();
+		})
+
+		//当视频元数据加载时运行
+		let totalTime = document.getElementById("totalTime");
+		player.addEventListener("loadedmetadata", () => {
+			//设置总时长
+			totalTime.innerText = this.getTimeBySecond(player.duration)
+		})
 	}
 
-	onClick = () => {
-		let player = document.getElementById("player");
-		let vedioPause = document.getElementById("videoPause")
-		if (player.paused) {
-			player.play()
-			vedioPause.style.display = "none";
-		} else {
-			player.pause();
-			vedioPause.style.display = "block"
-		}
+	//将当前秒数转换为时间
+	getTimeBySecond = (second) => {
+		var hour = parseInt(second / (60 * 60));
+		var minute = parseInt((second / 60) % 60);
+		var second = parseInt(second % 60);
+		return (hour < 10 ? "0" + hour : hour) + ":" + (minute < 10 ? "0" + minute : minute) + ":" + (second < 10 ? "0" + second : second);
 	}
 
 	componentWillUnmount() {
@@ -54,11 +94,21 @@ export default class VideoList extends BaseComponent {
 					preload="auto"
 					poster={location.query.background}
 					style={{ 'background': '#000' }}
-					onClick={this.onClick}
 
 				>
+
 					<source src={location.query.source} type="video/mp4"></source>
 				</video>
+				<div className="controls">
+					<div className="progress">
+						<div className="line" id='line'></div>
+						<div className="bar" id='bar'></div>
+					</div>
+					<div className="times">
+						<span className="current" id='current'>00:00:00</span>/
+						<span className="totalTime" id='totalTime'>00:00:00</span>
+					</div>
+				</div>
 			</div>
 		)
 	}
